bf2712ffb00d3bf2881716826d224872a806c9bf
kent winter <kgw700@gmail.com>
2017-11-12 04:21:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var name = "Joe"
-var height = "74"
-
-
-// Don't worry about this
-module.exports = { name, height
+var height = 74
+if (name === "Susan") {
+    console.log("The name is correct")
 }
 
+module.exports = { name, height
+}
